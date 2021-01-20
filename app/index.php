73534137281aa315fa14545c9e73f06005e6efab
tealord <mail@sezi.eu>
2021-01-20 21:03:06 +0100

    testing SMS function

--- app/index.php
+++ app/index.php
@@ -1,15 +1,15 @@
 <?php
 	include 'settings.php';
 	if (isset($_POST['left'])) {
-		shell_exec('printf "sid=0;cmd=set;direction=0\r" > ' . $SERIAL_PORT);
-		shell_exec('printf "sid=0;cmd=set;mode=0\r" > ' . $SERIAL_PORT);
+		shell_exec('printf "sid=0;cmd=set;direction=0;\r" > ' . $SERIAL_PORT);
+		shell_exec('printf "sid=0;cmd=set;mode=0;\r" > ' . $SERIAL_PORT);
 	}
 	if (isset($_POST['stop'])) {
-		shell_exec('printf "sid=0;cmd=set;mode=1\r" > ' . $SERIAL_PORT);
+		shell_exec('printf "sid=0;cmd=set;mode=1;\r" > ' . $SERIAL_PORT);
 	}
 	if (isset($_POST['right'])) {
-		shell_exec('printf "sid=0;cmd=set;direction=1\r" > ' . $SERIAL_PORT);
-		shell_exec('printf "sid=0;cmd=set;mode=0\r" > ' . $SERIAL_PORT);
+		shell_exec('printf "sid=0;cmd=set;direction=1;\r" > ' . $SERIAL_PORT);
+		shell_exec('printf "sid=0;cmd=set;mode=0;\r" > ' . $SERIAL_PORT);
 	}
 	if (isset($_POST['set-startpoint'])) {
 		shell_exec('printf "sid=0;cmd=set;startpoint;\r" > ' . $SERIAL_PORT);
@@ -20,6 +20,18 @@
 	if (isset($_POST['travel'])) {
 		shell_exec('printf "sid=0;cmd=set;mode=2;\r" > ' . $SERIAL_PORT);
 	}
+	if (isset($_POST['exposure_time_ms'])) {
+		shell_exec('printf "sid=0;cmd=set;exposure_time_ms=4000;\r" > ' . $SERIAL_PORT);
+	}
+	if (isset($_POST['exposure_time_offset_ms'])) {
+		shell_exec('printf "sid=0;cmd=set;exposure_time_offset_ms=2000;\r" > ' . $SERIAL_PORT);
+	}
+	if (isset($_POST['sms_pictures_amount'])) {
+		shell_exec('printf "sid=0;cmd=set;sms_pictures_amount=5;\r" > ' . $SERIAL_PORT);
+	}
+	if (isset($_POST['runSMS'])) {
+		shell_exec('printf "sid=0;cmd=set;runSMS=1;\r" > ' . $SERIAL_PORT);
+	}
 ?>
 
 <!DOCTYPE html>
@@ -29,6 +41,7 @@
 	</head>
 	<body>
 		<h1>Stepper Control Unit</h1>
+		<h2>Basic Controls</h2>
 		<form action="/index.php" method="post">
 			<input type="submit" name="left" value="Left">
 			<input type="submit" name="stop" value="Stop">
@@ -39,5 +52,12 @@
 			<br>
 			<input type="submit" name="travel" value="Start Travel">
 		</form> 
+		<h2>Shoot Move Shoot</h2>
+		<form action="/index.php" method="post">
+			<input type="submit" name="exposure_time_ms" value="Exposure 4000ms"><br>
+			<input type="submit" name="exposure_time_offset_ms" value="Offset 2000ms"><br>
+			<input type="submit" name="sms_pictures_amount" value="Pictures 5"><br>
+			<input type="submit" name="runSMS" value="Start"><br>
+		</form>
 	</body>
 </html>
