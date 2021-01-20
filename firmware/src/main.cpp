dbd835c4d0a30db32c07dfb7272a04963a9f0ed7
tealord <mail@sezi.eu>
2021-01-20 15:54:05 +0100

    move command processing from loop to readCMD() function

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -21,6 +21,7 @@
 #define	COUNTERCLOCKWISE	1
 #define STARTPOINT		0
 #define ENDPOINT		1
+#define CMD_MAXLENGHT		50
 #define MODE_RUN		0
 #define MODE_STOP		1
 #define MODE_TRAVEL		2
@@ -29,6 +30,9 @@ byte y_mode = MODE_STOP;
 long y_startpoint_distance = 0;
 long y_endpoint = 0;
 bool y_travel_destination = STARTPOINT;
+unsigned int y_speed = 500;
+byte cmd_char_cnt = 0;
+char cmd[CMD_MAXLENGHT];
 
 // counter and compare values
 // XXX do we need those?
@@ -77,15 +81,138 @@ void printUsage() {
 	Serial.println("sid=0;cmd=set;mode=2;");
 }
 
+void readCMD() {
+	if(Serial.available()){
+		char c = Serial.read();
+		if (cmd_char_cnt >= CMD_MAXLENGHT) {
+			Serial.println("error: your command is to long");
+			cmd_char_cnt = 0;
+			return;
+		}
+		if (c == '\r') {
+			if (cmd_char_cnt == 0) {
+				Serial.println("please enter command or type help");
+				return;
+			}
+			cmd[cmd_char_cnt] = '\0';
+			cmd_char_cnt = 0;
+		} else {
+			cmd[cmd_char_cnt] = c;
+			cmd_char_cnt++;
+				return;
+		}
+	} else {
+		return;
+	}
+
+	// split the command parts
+	char *cmdparts[7];
+	char *ptr = NULL;
+	byte index = 0;
+	ptr = strtok(cmd, ";="); // takes a list of delimiters
+	while (ptr != NULL) {
+		cmdparts[index] = ptr;
+		index++;
+		ptr = strtok(NULL, ";="); // takes a list of delimiters
+	}
+
+	// get stepper id
+	if (strcmp(cmdparts[0], "sid") != 0) {
+		Serial.println("error: no sid found");
+		printUsage();
+		return;
+	}
+
+	// get command
+	if (strcmp(cmdparts[2], "cmd") != 0) {
+		Serial.println("error: no cmd found");
+		printUsage();
+		return;
+	}
+
+	// run command: get
+	if (strcmp(cmdparts[3], "get") == 0) {
+		if (strcmp(cmdparts[4], "speed") == 0) {
+			Serial.println(y_speed);
+			return;
+		}
+		if (strcmp(cmdparts[4], "direction") == 0) {
+			Serial.println(digitalRead(Y_DIR));
+			return;
+		}
+		if (strcmp(cmdparts[4], "startpoint") == 0) {
+			Serial.println(y_startpoint_distance);
+			return;
+		}
+		if (strcmp(cmdparts[4], "endpoint") == 0) {
+			Serial.println(y_endpoint);
+			return;
+		}
+		if (strcmp(cmdparts[4], "mode") == 0) {
+			Serial.println(y_mode);
+			return;
+		}
+		Serial.print("error: '");
+		Serial.print(cmdparts[4]);
+		Serial.println("' is an unkown variable");
+		return;
+	}
+	// run command: set
+	if (strcmp(cmdparts[3], "set") == 0) {
+		if (strcmp(cmdparts[4], "speed") == 0) {
+			// XXX validate value
+			y_speed = atoi(cmdparts[5]);
+			setSpeed(y_speed);
+			return;
+		}
+		if (strcmp(cmdparts[4], "direction") == 0) {
+			// XXX validate value
+			digitalWrite(Y_DIR, atoi(cmdparts[5]));
+			return;
+		}
+		if (strcmp(cmdparts[4], "direction") == 0) {
+			// XXX validate value
+			digitalWrite(Y_DIR, atoi(cmdparts[5]));
+			return;
+		}
+		if (strcmp(cmdparts[4], "startpoint") == 0) {
+			y_startpoint_distance = 0;
+			y_mode = MODE_STOP;
+			return;
+		}
+		if (strcmp(cmdparts[4], "endpoint") == 0) {
+			y_endpoint = y_startpoint_distance;
+			y_mode = MODE_STOP;
+			return;
+		}
+		if (strcmp(cmdparts[4], "mode") == 0) {
+			// XXX validate value
+			y_mode = atoi(cmdparts[5]);
+			if (y_mode == MODE_TRAVEL) {
+				// XXX y_startpoint_distance must not be 0
+				if (y_startpoint_distance == 0) {
+					y_mode = MODE_STOP;
+				}
+			}
+			return;
+		}
+		Serial.print("error: '");
+		Serial.print(cmdparts[4]);
+		Serial.println("' is an unkown variable");
+		return;
+	}
+}
+
 void setup() {
 	Serial.begin(9600);
-	Serial.setTimeout(60000);
+	//Serial.setTimeout(60000);
 	Serial.println("Starting Firmware");
 	while(!Serial.available());
 	pinMode(Y_DIR, OUTPUT);
 	pinMode(Y_STP, OUTPUT);
 	pinMode(EN, OUTPUT);
 	digitalWrite(EN, LOW);
+	setSpeed(y_speed);
 
 	// Timer 1
 	noInterrupts();
@@ -113,121 +240,7 @@ void setup() {
 }
 
 void loop() {
-
-	byte cmd_lenght_max = 35;
-	char cmd[cmd_lenght_max];
-	
-	unsigned int y_speed = 500;
-	setSpeed(y_speed);
-
-	while (true) {
-		int cmd_lenght = Serial.readBytesUntil('\r', cmd, cmd_lenght_max);
-		if (cmd_lenght == cmd_lenght_max) {
-			Serial.println("error: your command is to long");
-			continue;
-		}
-		if (cmd_lenght == 0) {
-			Serial.println("please enter command or type help");
-			continue;
-		}
-
-		// split the command parts
-		char *cmdparts[7];
-		char *ptr = NULL;
-		byte index = 0;
-		ptr = strtok(cmd, ";="); // takes a list of delimiters
-		while (ptr != NULL) {
-			cmdparts[index] = ptr;
-			index++;
-			ptr = strtok(NULL, ";="); // takes a list of delimiters
-		}
-
-		// get stepper id
-		if (strcmp(cmdparts[0], "sid") != 0) {
-			Serial.println("error: no sid found");
-			printUsage();
-			continue;
-		}
-
-		// get command
-		if (strcmp(cmdparts[2], "cmd") != 0) {
-			Serial.println("error: no cmd found");
-			printUsage();
-			continue;
-		}
-
-		// run command: get
-		if (strcmp(cmdparts[3], "get") == 0) {
-			if (strcmp(cmdparts[4], "speed") == 0) {
-				Serial.println(y_speed);
-				continue;
-			}
-			if (strcmp(cmdparts[4], "direction") == 0) {
-				Serial.println(digitalRead(Y_DIR));
-				continue;
-			}
-			if (strcmp(cmdparts[4], "startpoint") == 0) {
-				Serial.println(y_startpoint_distance);
-				continue;
-			}
-			if (strcmp(cmdparts[4], "endpoint") == 0) {
-				Serial.println(y_endpoint);
-				continue;
-			}
-			if (strcmp(cmdparts[4], "mode") == 0) {
-				Serial.println(y_mode);
-				continue;
-			}
-			Serial.print("error: '");
-			Serial.print(cmdparts[4]);
-			Serial.println("' is an unkown variable");
-			continue;
-		}
-		// run command: set
-		if (strcmp(cmdparts[3], "set") == 0) {
-			if (strcmp(cmdparts[4], "speed") == 0) {
-				// XXX validate value
-				y_speed = atoi(cmdparts[5]);
-				setSpeed(y_speed);
-				continue;
-			}
-			if (strcmp(cmdparts[4], "direction") == 0) {
-				// XXX validate value
-				digitalWrite(Y_DIR, atoi(cmdparts[5]));
-				continue;
-			}
-			if (strcmp(cmdparts[4], "direction") == 0) {
-				// XXX validate value
-				digitalWrite(Y_DIR, atoi(cmdparts[5]));
-				continue;
-			}
-			if (strcmp(cmdparts[4], "startpoint") == 0) {
-				y_startpoint_distance = 0;
-				y_mode = MODE_STOP;
-				continue;
-			}
-			if (strcmp(cmdparts[4], "endpoint") == 0) {
-				y_endpoint = y_startpoint_distance;
-				y_mode = MODE_STOP;
-				continue;
-			}
-			if (strcmp(cmdparts[4], "mode") == 0) {
-				// XXX validate value
-				y_mode = atoi(cmdparts[5]);
-				if (y_mode == MODE_TRAVEL) {
-					// XXX y_startpoint_distance must not be 0
-					if (y_startpoint_distance == 0) {
-						y_mode = MODE_STOP;
-					}
-				}
-				continue;
-			}
-			Serial.print("error: '");
-			Serial.print(cmdparts[4]);
-			Serial.println("' is an unkown variable");
-			continue;
-		}
-	}
+	readCMD();
 }
 
 ISR(TIMER1_COMPA_vect) {
